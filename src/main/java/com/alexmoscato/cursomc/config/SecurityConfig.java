08534f1ecd068b2b76a763070703672171fbe3d5
alex9044 <alexmoscato9044@gmail.com>
2022-03-15 09:14:27 -0300

    Configuracao inicial do SpringSecurity

---
+++ src/main/java/com/alexmoscato/cursomc/config/SecurityConfig.java
@@ -0,0 +1,61 @@
+package com.alexmoscato.cursomc.config;
+
+import java.util.Arrays;
+
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpMethod;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	private Environment env;
+
+	// Criando vetor de endpoints que podem ser acessados
+	private static final String[] PUBLIC_MATCHERS = { "/h2/console/**" };
+
+	// Criando vetor de endpoints que podem ser acessados somente para leitura.
+	private static final String[] PUBLIC_MATCHERS_GET = { "/produtos/**", "/categorias/**" };
+
+	//
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+
+		// Liberando acesso ao banco de dados h2
+		// Test
+		if (Arrays.asList(env.getActiveProfiles()).contains("test")) {
+			http.headers().frameOptions().disable();
+		}
+		// Prod
+		if (Arrays.asList(env.getActiveProfiles()).contains("prod")) {
+			http.headers().frameOptions().disable();
+		}
+		// Chamando cors e deshabilitando protecao csrf
+		http.cors().and().csrf().disable();
+		// Definindo acesso a endpoints
+		http.authorizeRequests().antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
+				.antMatchers(PUBLIC_MATCHERS).permitAll().anyRequest().authenticated();
+		// Desabilitando criacao de usuario.
+		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+	}
+
+	// Permintindo acesso aos endpoints por multipla fontes.
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+		return source;
+	}
+
+}
